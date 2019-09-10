e2e061a7207fd7c6fe362afbf0586ae7db1bf32c
Sumit Srivastava <ssrivas8@ncsu.edu>
2019-09-09 20:23:53 -0700

    Code for 1.7.3

--- 1.LearningNodeJs/07/server.js
+++ 1.LearningNodeJs/07/server.js
@@ -20,27 +20,26 @@ app.get('/messages', (req, res) => {
   });
 });
 
-app.post('/messages', (req, res) => {
+app.post('/messages', async (req, res) => {
   var message = new Message(req.body);
 
-  message
-    .save()
-    .then(() => {
-      console.log('saved');
-      return Message.findOne({ message: 'badword' });
-    })
-    .then(censored => {
-      if (censored) {
-        console.log('censored word found', censored);
-        return Message.remove({ _id: censored.id });
-      }
-      io.emit('message', req.body);
-      res.sendStatus(200);
-    })
-    .catch(err => {
-      res.sendStatus(500);
-      return console.log(err);
-    });
+  var savedMessage = await message.save();
+
+  console.log('saved');
+
+  var censored = await Message.findOne({ message: 'badword' });
+
+  if (censored) {
+    await Message.remove({ _id: censored.id });
+  } else {
+    io.emit('message', req.body);
+  }
+  res.sendStatus(200);
+
+  // .catch(err => {
+  //   res.sendStatus(500);
+  //   return console.log(err);
+  // });
 });
 
 io.on('connection', socket => {
